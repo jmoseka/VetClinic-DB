f870ab037611da30ab9592ce48ebd90ceff02453
jmoseka <jamylam3@gmail.com>
2022-07-05 14:41:19 +0300

    List the name of all animals born between 2016 and 201

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
+/* List the name of all animals born between 2016 and 2019*/
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
