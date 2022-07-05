44906f86feab765c49aa05bfc615e68d83481d5f
jmoseka <jamylam3@gmail.com>
2022-07-05 14:48:05 +0300

    List the date of birth of all animals named either Agumon or Pikachu

--- queries.sql
+++ queries.sql
@@ -5,3 +5,6 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 /* Name of all animals that are neutered and have less than 3 escape attempts */
 SELECT * FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+/* List the date of birth of all animals named either "Agumon" or "Pikachu"*/
+SELECT * FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
