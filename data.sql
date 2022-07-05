6d98cd4f511820dfa2d3b119e867e6b3d3aa17b9
jmoseka <jamylam3@gmail.com>
2022-07-05 12:38:49 +0300

    Insert data into the table

--- data.sql
+++ data.sql
@@ -3,3 +3,12 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Agumon', '2020-02-03', 10.3, TRUE,0);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Gabumon', '2018-11-15', 8, TRUE, 2);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Pikachu', '2021-01-07', 15.04, FALSE, 1);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Devimon', '2018-05-12', 11, TRUE, 5);
