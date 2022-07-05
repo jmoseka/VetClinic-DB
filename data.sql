d1f21a9555a022d8c17d686a83a4de16869cf3b6
jmoseka <jamylam3@gmail.com>
2022-07-05 16:24:23 +0300

    Update file

--- data.sql
+++ data.sql
@@ -1,9 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
-
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES('Agumon', '2020-02-03', 10.3, TRUE,0);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT * FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
-SELECT * FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name FROM animals WHERE weight_kg > 10;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
