7d6030751702f1bcd1f5344c4a1a1584bd0df9bc
jmoseka <jamylam3@gmail.com>
2022-07-06 20:29:51 +0300

    Commit transaction

--- data.sql
+++ data.sql
@@ -62,11 +62,12 @@ SAVEPOINT point1;
 UPDATE animals
 SET weight_kg = weight_kg * -1;
 /*Rollback to the savepoint*/
-ROLLBACK point1;
+ROLLBACK TO point1;
 /*Update all animals' weights that are negative to be their weight multiplied by -1*/
 UPDATE animals
 SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-
+/*Commit transaction*/
+COMMIT;
 
 
 
