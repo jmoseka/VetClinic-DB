1c29b14c56c8bab9171963a389a3549277298ec8
jmoseka <jamylam3@gmail.com>
2022-07-07 15:22:29 +0300

    Add column species_id which is a foreign key referencing species table

--- schema.sql
+++ schema.sql
@@ -27,3 +27,8 @@ name VARCHAR(100)
 -- Remove column species
 ALTER TABLE animals
 DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD COLUMN IF NOT EXISTS species_id INT
+ALTER TABLE animals
+ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
