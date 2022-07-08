9463b4cbe6678c6bc098955398796ae661c746a3
jmoseka <jamylam3@gmail.com>
2022-07-08 16:41:45 +0300

    Insert data in visits

--- data.sql
+++ data.sql
@@ -218,3 +218,128 @@ VALUES(
 (SELECT id FROM species WHERE name = 'Digimon'),
 (SELECT id FROM vets WHERE name = 'Jack Harkness')
 );
+
+
+-- Insert the following data for visits:
+-- Agumon visited William Tatcher on May 24th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Agumon'),
+(SELECT id FROM vets WHERE name = 'William Tatcher'),
+'2020-05-24'
+);
+
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Agumon'),
+(SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+'2020-07-22'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Gabumon'),
+(SELECT id FROM vets WHERE name = 'Jack Harkness'),
+'2021-02-02'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Pikachu'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2020-01-05'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Pikachu'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2020-03-08'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Pikachu'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2020-05-14'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Devimon'),
+(SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+'2021-05-04'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Charmander'),
+(SELECT id FROM vets WHERE name = 'Jack Harkness'),
+'2021-02-24'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Plantmon'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2019-12-21'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Plantmon'),
+(SELECT id FROM vets WHERE name = 'William Tatcher'),
+'2020-08-10'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Plantmon'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2021-04-07'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Squirtle'),
+(SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+'2019-09-29'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Angemon'),
+(SELECT id FROM vets WHERE name = 'Jack Harkness'),
+'2020-10-03'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Angemon'),
+(SELECT id FROM vets WHERE name = 'Jack Harkness'),
+'2020-11-04'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Boarmon'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2019-01-24'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Boarmon'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2019-05-15'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Boarmon'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2020-02-27'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Boarmon'),
+(SELECT id FROM vets WHERE name = 'Maisy Smith'),
+'2020-08-03'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Blossom'),
+(SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+'2020-05-24'
+);
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES(
+(SELECT id FROM animals WHERE name = 'Blossom'),
+(SELECT id FROM vets WHERE name = 'William Tatcher'),
+'2021-01-11'
+);
