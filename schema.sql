87c03277d4be8eb77ef4448431f7b28010636d4b
jmoseka <jamylam3@gmail.com>
2022-07-08 14:44:29 +0300

    Add join table to handle a many-to-many relationship between animals and vets

--- schema.sql
+++ schema.sql
@@ -53,3 +53,13 @@ vets_id INT,
 CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
 CONSTRAINT fk_vets FOREIGN KEY (vets_id) REFERENCES vets(id)
 );
+
+-- Create join table to handle a many-to-many relationship between species and vet
+CREATE TABLE visits (
+id SERIAL PRIMARY KEY,
+animals_id INT,
+vets_id INT,
+date_of_visit DATE,
+CONSTRAINT fk_animals FOREIGN KEY (animals_id) REFERENCES animals(id),
+CONSTRAINT fk_vets FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
